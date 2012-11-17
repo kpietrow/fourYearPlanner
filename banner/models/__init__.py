2b754502dd328b2b039d3189e4ad63a1ff3b4dff
Travis Beatty <travisby@gmail.com>
2012-11-17 16:53:48 -0500

    Added banner app

---
+++ banner/models/__init__.py
@@ -0,0 +1,13 @@
+from minor import Minor
+from major import Major
+from professor import Professor
+from user_session_track import User_Session_Track
+from session import Session
+from section_time import Section_Time
+from semester import Semester
+from prerequisite import Prerequisite
+from course import Course
+from major_requirement import Major_Requirement
+from minor_requirement import Minor_Requirement
+from phone import Phone
+
