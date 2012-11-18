285e422c83c538359418bad5bef79ab748c7a4de
kpietrow <kpietrow@gmail.com>
2012-11-18 14:42:23 -0500

    All renders in view turned to HttpResponse, and HttpResponse's parameters were changed accordingly

--- banner/views.py
+++ banner/views.py
@@ -1,10 +1,8 @@
 from banner.models import Course, Section, User_Section_Track, Major, Minor
 from django.http import HttpResponse, Http404, HttpResponseRedirect
-from django.shortcuts import render
-
 
 def index(request):
-    return render(request, template_name='index.html')
+    return HttpResponse(request, template_name='index.html')
 
 def track_section(request, user_name, section_id):
     """Track section"""
@@ -13,63 +11,53 @@ def track_section(request, user_name, section_id):
     return HttpResponse('Your course tracking has been successfully activated.')
 
 
-def courses_by_major(request):
-    """Look up major courses"""
+def courses_by_major_id(request, major_id):
+    """Look up major courses by its ID"""
     try:
-        courses_by_major = []
-        for major in Major.objects.all():
-            courses = Course.objects.filter(major_id__exact=major.pk)
-            courses.prereq = [x.name for x in courses.prerequisite_set]
-            courses_by_major.append(courses)
-
-        return render(
-            request,
-            template_name='sections/degree_reqs.html',
-            dictionary={
+        courses = Course.objects.filter(major_id__exact=major_id)
+        courses.prereq = [x.name for x in courses.prerequisite_set]
+        return HttpResponse(
+            {
                 'degree': courses,
-                'location': 'prereqs.html'
+                'location': 'degree_reqs.html'
             }
         )
     except ValueError:
         raise Http404()
 
 
-def courses_by_minor(request):
+def courses_by_minor_id(request, minor_id):
     """Look up minor courses by its ID"""
     try:
-        courses_by_minor = []
-        for minor in Minor.objects.all():
-            courses = Course.objects.filter(minor_id__exact=minor.pk)
-            courses.prereq = [x.name for x in courses.prerequisite_set]
-            courses_by_minor.append(courses)
-
-        return render(
-            request,
-            template_name='sections/degree_reqs.html',
-            dictionary={
+        courses = Course.objects.filter(minor_id__exact=minor_id)
+        for course in courses:
+            courses.prereqs = [x.name for x in courses.prerequisite_set]
+        return HttpResponse(
+            {
                 'degree': courses,
-                'location': 'prereqs'
+                'location': 'degree_reqs.html'
             }
         )
     except ValueError:
         raise Http404()
 
-def search_by_course_id(request, course_id):
-    sections = Section.objects.filter(course_id__exact=course_id)
-    return render(
-        request,
-        template_name='sections/lvSections_cID.html',
-        dictionary={
-            'sections': sections,
-            'location': 'lvSections_cID.html'
-        }
-    )
+
+def all_courses(request, major_id, minor_id):
+    """Look up all courses for major and minor"""
+    response = Course.objects.filter(minor_id__exact=minor_id) + Course.objects.filter(major_id__exact=major_id)
+    response = set(response)
+    response = HttpResponse(response)
+    return response
+
+
+def sections_by_semester(request, semester_id):
+    """Look up all sections for semester"""
+    response = HttpResponse(Section.objects.filter(semester_id__exact=semester_id))
+    return response
 
 def schedule(request):
-    return render(
-        request,
-        template_name='sections/schedule.html',
-        dictionary={
+    return HttpResponse(
+        {
             'sections': Section.objects.filter(user__exact=request.user),
             'user': request.user
         }
